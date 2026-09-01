24b5b794974dffea62c1d3ed6f31b894f1dea93b
radfan abdullah <radfanr22@gmail.com>
2026-09-01 16:18:05 +0300

    test: accept duplicate app title widgets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,6 @@ void main() {
   testWidgets('App launches and shows title', (WidgetTester tester) async {
     await tester.pumpWidget(const GeoScanAI());
     await tester.pumpAndSettle();
-    expect(find.text('GeoScan AI'), findsOneWidget);
+    expect(find.text('GeoScan AI'), findsWidgets);
   });
 }
